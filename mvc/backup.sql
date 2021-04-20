b6f78a4bb69ede77e0b9545654e1af39ec41a506
Edwin Becerra <edwin.becerra@uniminuto.edu.co>
2021-04-20 15:28:25 -0500

    Update backup.sql
    Se modifica la base de datos, y su backup en el proyecto

--- mvc/backup.sql
+++ mvc/backup.sql
@@ -1,53 +1,145 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         5.6.21 - MySQL Community Server (GPL)
--- SO del servidor:              Win32
--- HeidiSQL Versión:             9.1.0.4903
--- --------------------------------------------------------
+CREATE DATABASE  IF NOT EXISTS `colegio` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
+USE `colegio`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: colegio
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.17-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Volcando estructura de base de datos para colegio
-CREATE DATABASE IF NOT EXISTS `colegio` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci */;
-USE `colegio`;
+--
+-- Table structure for table `alumno_curso`
+--
 
+DROP TABLE IF EXISTS `alumno_curso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumno_curso` (
+  `Curso_id` int(11) NOT NULL,
+  `Alumno_id` int(11) NOT NULL,
+  PRIMARY KEY (`Curso_id`,`Alumno_id`),
+  KEY `Alumno_id` (`Alumno_id`),
+  CONSTRAINT `alumno_curso_ibfk_1` FOREIGN KEY (`Alumno_id`) REFERENCES `alumnos` (`id`),
+  CONSTRAINT `alumno_curso_ibfk_2` FOREIGN KEY (`Curso_id`) REFERENCES `cursos` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando estructura para tabla colegio.alumnos
-CREATE TABLE IF NOT EXISTS `alumnos` (
+--
+-- Dumping data for table `alumno_curso`
+--
+
+LOCK TABLES `alumno_curso` WRITE;
+/*!40000 ALTER TABLE `alumno_curso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alumno_curso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alumnos`
+--
+
+DROP TABLE IF EXISTS `alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumnos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(50) NOT NULL DEFAULT '0',
   `Apellido` varchar(50) NOT NULL DEFAULT '0',
-  `Sexo` tinyint(4) NOT NULL DEFAULT '0',
+  `Sexo` tinyint(4) NOT NULL DEFAULT 0,
   `FechaNacimiento` varchar(20) DEFAULT NULL,
   `FechaRegistro` varchar(20) DEFAULT NULL,
   `Correo` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
+--
+-- Dumping data for table `alumnos`
+--
 
+LOCK TABLES `alumnos` WRITE;
+/*!40000 ALTER TABLE `alumnos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alumnos` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Volcando estructura para tabla colegio.alumno_curso
-CREATE TABLE IF NOT EXISTS `alumno_curso` (
-  `Curso_id` int(11) NOT NULL,
-  `Alumno_id` int(11) NOT NULL,
-  PRIMARY KEY (`Curso_id`,`Alumno_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+--
+-- Table structure for table `cursos`
+--
 
--- La exportación de datos fue deseleccionada.
-
-
--- Volcando estructura para tabla colegio.cursos
-CREATE TABLE IF NOT EXISTS `cursos` (
+DROP TABLE IF EXISTS `cursos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cursos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(500) COLLATE utf8_spanish_ci NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  `idProfesor` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`idProfesor`),
+  KEY `idProfesor` (`idProfesor`),
+  CONSTRAINT `cursos_ibfk_1` FOREIGN KEY (`idProfesor`) REFERENCES `profesores` (`idProfesor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- La exportación de datos fue deseleccionada.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+--
+-- Dumping data for table `cursos`
+--
+
+LOCK TABLES `cursos` WRITE;
+/*!40000 ALTER TABLE `cursos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cursos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesores`
+--
+
+DROP TABLE IF EXISTS `profesores`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `profesores` (
+  `idProfesor` int(11) NOT NULL,
+  `Nombre` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `Apellido` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  `Sexo` varchar(45) COLLATE utf8_spanish_ci NOT NULL,
+  `FechaNacimiento` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `FechaRegistro` varchar(20) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `Correo` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idProfesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesores`
+--
+
+LOCK TABLES `profesores` WRITE;
+/*!40000 ALTER TABLE `profesores` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profesores` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'colegio'
+--
+
+--
+-- Dumping routines for database 'colegio'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-04-20 15:26:34
